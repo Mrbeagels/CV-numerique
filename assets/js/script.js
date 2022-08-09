417443c8f99f1cc9289ced410e8bf12878492e5c
Thibaud Marin <thibaud.marin1@gmail.com>
2022-08-09 17:13:03 +0200

    Mise a jour du mode téléphone grace a une modal

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+let expModal = document.querySelector('.expModal');
+expModal.addEventListener('click', () => {
+    modalToggle.show();
+});
+
